bcdfed9c46a2a6dd5402ede65f52cebd7c1ccd07
Krists Sturmanis <s253191@dtu.dk>
2026-03-10 11:34:05 +0100

    I added the assignment doc, the game rules from the internet and my .py file where I will code only the game board (to be done until next week

---
+++ Game_Board_code.py
@@ -0,0 +1,4 @@
+"This file contains the code for the game board"
+
+"Game board is a 16 x 16 grid of tiles, each tile can be empty, be surrounded by a wall from one or two sides, a player (robot), or a goal"
+
